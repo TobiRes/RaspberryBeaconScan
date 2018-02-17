b884234c3d35625e4c7fa924587e4cc16508dc9e
5heinzel <5heinzel@informatik.uni-hamburg.de>
2018-02-17 16:11:48 +0100

    post calculated distance

--- testblescan.py
+++ testblescan.py
@@ -1,19 +1,21 @@
-import json
-import time
-import bluetooth._bluetooth as bluez
-import requests
-import blescan
 import datetime
+import json
+import math
 import time
 from uuid import getnode as get_mac
+
+import bluetooth._bluetooth as bluez
+import requests
+
+import blescan
 
 all_macs = []
 pi_mac = get_mac()
 
 dev_id = 0
+
 try:
     sock = bluez.hci_open_dev(dev_id)
-    print("ble thread started")
 
 except:
     print("error accessing bluetooth device...")
@@ -21,29 +23,45 @@ except:
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
-server_url = 'http://10.200.20.150:8080'
+server_url = 'http://10.200.18.137:8080'
 
 while True:
-	r = requests.get(url=server_url + '/beacon')
-	beacons = json.loads(r.text)
-	for beacon in beacons:
-		all_macs.append(beacon['macAddress'])
-	print("Registrierte Beacons aktualisiert:")
-	print(all_macs)
-	for x in range(0, 20):
-		returnedList = blescan.parse_events(sock, 10)
-		print("Bluetooth Geraete gefunden.")
-		for bluetoothDevice in returnedList:
-			for mac in all_macs:
-				if bluetoothDevice['mac'] and mac.lower() == bluetoothDevice['mac'].lower():
-					print("success", mac.lower())
-					print(bluetoothDevice)
-					ts = time.time()	
-					timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-					payload = { "senderID": str(pi_mac), "beaconID": bluetoothDevice['mac'], "distanceToBeacon": 999, "timestamp": timestamp }
-             				headers = {"Content-Type": "application/json"}
-             				r = requests.post(server_url + '/distance', data=json.dumps(payload), headers=headers)
-					print(r.status_code)
-					print(r.text)
-		time.sleep(1)
-	time.sleep(300)
+
+    r = requests.get(url=server_url + '/beacon')
+    beacons = json.loads(r.text)
+    for beacon in beacons:
+        all_macs.append(beacon['macAddress'])
+    print("Registrierte Beacons aktualisiert:")
+    print(all_macs)
+    rssi_sum = 0.0
+    count = 0.0
+
+    for x in range(0, 20):
+        returnedList = blescan.parse_events(sock, 10)
+        print("Scanne Bluetooth Geraete... (" + str(x) + "/20)")
+        for bluetoothDevice in returnedList:
+            for mac in all_macs:
+                if bluetoothDevice['mac'] and mac.lower() == bluetoothDevice['mac'].lower():
+                    print("MAC erkannt:", mac.lower())
+                    print(bluetoothDevice)
+                    rssi_sum += bluetoothDevice['rssi']
+                    txp = bluetoothDevice['txp']
+                    count += 1.0
+
+    rssi_avg = rssi_sum / count
+    distance = math.pow(10, ((txp - rssi_avg) / (10 * 2.75)))
+
+    print('rssi_avg:', rssi_avg)
+    print('txp:', txp)
+    print('calculated distance:', distance)
+
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+    payload = {"senderID": str(pi_mac), "beaconID": bluetoothDevice['mac'], "distanceToBeacon": distance,
+               "timestamp": timestamp}
+    headers = {"Content-Type": "application/json"}
+
+    r = requests.post(server_url + '/distance', data=json.dumps(payload), headers=headers)
+    print(r.status_code)
+    time.sleep(10)
